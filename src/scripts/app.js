99d1b15b0bfc58f82600b139331c3e1ffba9c691
Claire <claire.ristow@gmail.com>
2018-09-16 16:39:50 -0400

    hamburger menu functioning - styles mostly finished

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,16 @@
 const hello = 'World';
 console.log(hello);
+
+// STICKY HEADER
+window.onscroll = function () { myFunction() };
+const header = document.getElementById("nav");
+const sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
